47e13a2813587122f9bf2072e8c8dafdfaefc3ac
AZZOZ8000 <120444134+AZZOZ8000@users.noreply.github.com>
2023-02-10 18:17:54 +0300

    Add files via upload

---
+++ 21.py
@@ -0,0 +1,17 @@
+
+class node:
+    def __init__ (self, dataval=None):
+        self.dataval=dataval
+        self.nextva=None
+class slinklist:
+    def init_ (self):
+        self.headval=None
+def listprint (self):
+    printval=self.headval
+while printval is not None:
+    print (printval.nextval)
+printval = printval.nextval
+list=slinklist ()
+list.headval=Node ('mon ' )
+e2=node ('Tus')
+e3=node ('wed')

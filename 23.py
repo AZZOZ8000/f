1313901d3d7171b24109483da0cb9f4605d1b8a1
AZZOZ8000 <120444134+AZZOZ8000@users.noreply.github.com>
2023-02-10 18:21:13 +0300

    Add files via upload

---
+++ 23.py
@@ -0,0 +1,23 @@
+
+class node:
+    def __init__ (self, dataval=None):
+         self.dataval=dataval 
+         self.nextva=None
+class slinklist:
+    def init_ (self):
+        self.headval=None
+    def listprint (self):
+        printval=self.headval
+while printval is not None:
+    print (printval.nextval)
+printval = printval.nextval
+list=slinklist ()
+list.headval=Node ('mon ' )
+e2=node ('Tus')
+e3=node ('wed')
+e4=( 'thu' )
+e5=('fri')
+800008
+list.hadval.nextval=e2
+e2.nextval=e3 
+list.listprint ()
---
+++ script.js
@@ -0,0 +1,9 @@
+function openfunction() {
+    document.getElementById('menu').style.width = '250px';
+
+}
+
+function closefunction() {
+    document.getElementById('menu').style.width = '0px';
+
+}
